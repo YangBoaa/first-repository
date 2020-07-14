9ed9ec2ff2aea9400fff52013d2bef6b3e3bfc7b
YangBoaa <boaa1004@nate.com>
2020-07-14 19:42:59 +0900

    commit

---
+++ boa/univ/2-3.py
@@ -0,0 +1,10 @@
+a = 100
+b = 50
+result = a + b
+print(a, "+", b, "=", result)
+result1 = a - b
+print(a, "-", b, "=", result1)
+result2 = a * b
+print(a, "*", b, "=", result2)
+result3 = a / b
+print(a, "/", b, "=", result3)
