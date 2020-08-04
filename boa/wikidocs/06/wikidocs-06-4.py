434fe306752974615041abb43001e88b602db8b1
JunhyunJunhyun <sji010900@gmail.com>
2020-08-04 17:03:09 +0900

    첨삭

--- boa/wikidocs/06/wikidocs-06-4.py
+++ boa/wikidocs/06/wikidocs-06-4.py
@@ -3,13 +3,13 @@ import sys
 
 option = sys.argv[1]
 
-if option == "-a":
+if option == "-a":  # 메모장 내용 추가
     memo = sys.argv[2]
     f = open("memo.txt", "a")
     f.write(memo)
     f.write("\n")
     f.close()
-elif option == "-v":
+elif option == "-v":  # 메모장 내용 읽기
     f = open("memo.txt")
     memo = f.read()
     f.close()
