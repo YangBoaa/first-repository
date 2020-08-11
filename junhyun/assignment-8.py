3256d936a818ba333f7483728cd1ad6c49db2616
JunhyunJunhyun <sji010900@gmail.com>
2020-08-11 17:53:03 +0900

    첨삭

--- junhyun/assignment-8.py
+++ junhyun/assignment-8.py
@@ -6,13 +6,17 @@ import json
 url = "https://schoolmenukr.ml/api/high/B100000593?year=2018&month=5"
 response = requests.get(url)
 
+print("=" * 25)
+print(response)
+print(dir(response))
+print("=" * 25)
+
 json_data = json.loads(response.text)
 data = json_data["menu"]
 lunch_list_raw = []
 
 for a in range(0, len(data)):
     lunch_list_raw.append(data[a]["lunch"])
-print(lunch_list_raw)
 
 lunch_list_raw = list(filter(None, lunch_list_raw))
 lunch_list = []
@@ -21,7 +25,7 @@ for lunch in lunch_list_raw:
     for a in range(0, len(lunch)):
         lunch[a] = re.sub("[^a-zA-Zㄱ-힗]", "", lunch[a])
         lunch_list.append(lunch[a])
-print(lunch_list)
+
 count = {}
 
 for menu in lunch_list:
