7e9a4092a6f8bd3e022f083ba6da8cc0abdb7fc9
JunhyunJunhyun <sji010900@gmail.com>
2020-08-11 20:22:50 +0900

    첨삭

--- boa/assignment/assignment-7.py
+++ boa/assignment/assignment-7.py
@@ -5,37 +5,34 @@ import requests
 
 url = "https://schoolmenukr.ml/api/high/B100000593?year=2018&month=5"
 response = requests.get(url)
-# with문과 json모듈을 이용하면 dictionary로 쉽게 사용 가능
 
 json_data = json.loads(response.text)  # json 파일을 읽어서 파싱하고 사용
 menu_list = json_data["menu"]
 
-
-lunch_list = []
+daily_lunch_list = []
 for i in range(0, len(menu_list)):  # for문으로 0부터 menu_list의 길이 만큼 돌림
     rule = re.compile("[가-힣]+")  # 한글 추출
     result = rule.findall(str(menu_list[i]))  # 문자열의 단어를 정규식과 매치하여 리스트로 돌려줌
     result1 = ",".join(result).split(",")
-    lunch_list.append(result)  # 리스트의 result 추가
+    daily_lunch_list.append(result)  # 리스트의 result 추가
 
-lunch_list = list(filter(None, lunch_list))  # None를 걸러주는 코드
-print(lunch_list)
+daily_lunch_list = list(filter(None, daily_lunch_list))  # None를 걸러주는 코드
+
+print(daily_lunch_list)
+
 final_lunch_list = []
-for a in lunch_list:  # lunch_list가 a 변수에 대입
-    for b in a:  # a가 b변수에 대입
-        final_lunch_list.append(b)  # 새로운 리스트에 추가
-
+for lunch_list in daily_lunch_list:  # lunch_list가 lunch_list 변수에 대입
+    for lunch in lunch_list:  # lunch_list가 b변수에 대입
+        final_lunch_list.append(lunch)  # 새로운 리스트에 추가
 
 count = {}  # 개수 세기
-for v in final_lunch_list:  # final_lunch_list가 v 변수에 대입
+for i in final_lunch_list:  # final_lunch_list가 i 변수에 대입
     try:
-        count[v] += 1
+        count[i] += 1
     except:
-        count[v] = 1
+        count[i] = 1
 
 # final_lunch_list의 중복된 개수대로 1씩 증가
-
-
 sdict = sorted(
     count.items(), key=lambda y: y[1], reverse=True
 )  # count를 순서대로 정렬하고 내림차순으로 리스트를 뒤집는다)
