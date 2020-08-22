c18b8ebd2cde9d0c623ad50706a36cc63b46e49e
YangBoaa <boaa1004@nate.com>
2020-08-22 14:18:35 +0900

    급식

--- boa/assignment/assignment-7.py
+++ boa/assignment/assignment-7.py
@@ -11,12 +11,32 @@ import datetime
 
 url = "https://schoolmenukr.ml/api/high/B100000593?"
 
-qurey = input("날짜를 입력하세요: ")
+query = input("날짜를 입력하세요: ")
+query = query.split("-")
 
-qurey = qurey.split("-")
-year = int(qurey[0])
-month = int(qurey[1])
-date = int(qurey[2])
+if len(query) == 1:
+    year = int(query[0])
+    month = None
+    date = None
+elif len(query) == 2:
+    year = int(query[0])
+    month = int(query[1])
+    date = None
+elif len(query) == 3:
+    year = int(query[0])
+    month = int(query[1])
+    date = int(query[2])
+else:
+    print("Error")
+    raise Exception
+
+print("알러지를 입력 받겠습니까?\n 1: 네  2: 아니요\n")
+allergy = input()
+
+if allergy == "1":
+    url += "allergy=formed&"
+elif allergy == "2":
+    url += "allergy=hidden&"
 
 if year is not None and month is not None and date is not None:
     url += f"year={year}&month={month}&date={date}"
--- boa/assignment/test.py
+++ boa/assignment/test.py
@@ -5,3 +5,12 @@ day = None
 
 print(date.split("-"))
 
+if qurey == month is None and date is None:
+    qurey = year
+elif qurey == date is None:
+    qurey = year + month
+
+if month is not None and year is not None:
+    print(year + month)
+elif year is not None:
+    print(year)
